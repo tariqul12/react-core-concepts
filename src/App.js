31f79ac072674c03f1e85584c032ffb5c0773910
tariqul12 <mshimul591@gmail.com>
2021-12-06 22:08:03 +0600

    resct vasice

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   const men = ['Anoar','Jafor','Salmansa', 'Alomgir','Bapparaj'];
+   const women=['Bobita','Sabnor','Moushomi','seka']
+   const job=['FoodBall','dorsok','banker','programer','houseWife','SekaKhaoa']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+       
+        <p>my first react paragraph</p>
+        <Person name={men[0]} job={job[0]}></Person>
+        <Person name={men[4]} job={job[5]}></Person>
+        <Person name={men[2]} job={job[3]}></Person>
+        <Person name={men[3]} job={job[2]}></Person>
+        <Person name={women[0]} job={job[4]}></Person>
+
+    
       </header>
     </div>
   );
+}
+ 
+function Person(props){
+  const personStyle={
+    border:'2px solid yellow',
+    margin:'20px',
+    width:'500px',
+  }
+  return (
+    <div style={personStyle}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
+    </div>
+  )
 }
 
 export default App;
