b59e3c6beab5f03b47663fed9e5272e94d900c4f
tariqul12 <mshimul591@gmail.com>
2021-12-08 12:11:55 +0600

    friends push

--- src/App.js
+++ src/App.js
@@ -2,38 +2,81 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-   const men = ['Anoar','Jafor','Salmansa', 'Alomgir','Bapparaj'];
-   const women=['Bobita','Sabnor','Moushomi','seka']
-   const job=['FoodBall','dorsok','banker','programer','houseWife','SekaKhaoa']
-  return (
+  const nayoks=['Razzak','jasim','Alomgir','Salman','Bappi','Sovo','Sakib','Manna']
+//   const products=[
+//     {name:'PhotoShop',price:'$90.89'},
+//     {name:'Illustrator',price:'$60.99'},
+//     {name:'PDF Reader', price:'$6.99'},
+//     {name:'AdobPhotoshop', price:'$123.45'},    
+// ] 
+const friends=[
+  {name:'Mamun',age:'23',job:'src'},
+  {name:'Niloy',age:'22',job:'Army'},
+  {name:'Emran',age:'21',job:'Student'},
+  {name:'Shakil',age:'24',job:'Forener'}
+]
+//  const productName=products.map(product=> product.name)
+//  console.log(productName);
+//  const nayokName=nayoks.map(nayok=>nayok)
+//  console.log(nayokName);
+
+   return (
     <div className="App">
       <header className="App-header">
        
         <p>my first react paragraph</p>
-        <Person name={men[0]} job={job[0]}></Person>
-        <Person name={men[4]} job={job[5]}></Person>
-        <Person name={men[2]} job={job[3]}></Person>
-        <Person name={men[3]} job={job[2]}></Person>
-        <Person name={women[0]} job={job[4]}></Person>
 
-    
+        {
+          friends.map(friend=> <h3>Name: {friend.name}</h3>)
+        }
+        {
+          friends.map(friend=> <Product friend={friend}></Product>)
+        }
+{/* 
+        <ul>
+          {
+            nayoks.map(nayok=> <li>{nayok}</li>)
+          }
+          {
+            products.map(product=><li>{product.name}</li>)
+          }
+          {
+            products.map(product=> <li>price:{product.price}</li>)
+          }
+        </ul>
+        {
+          products.map(product=> <Product product={product}></Product>)
+        } */}
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product> */}
       </header>
     </div>
   );
 }
- 
-function Person(props){
-  const personStyle={
-    border:'2px solid yellow',
-    margin:'20px',
-    width:'500px',
+
+function Product(props){
+  const productStyle={
+    border:'2px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightGray',
+    height:'300px',width:'200px',
+    color:'#000',
+    margin:'30px'
   }
+  // const {name,price} =props.product;
+  const {name,age,job}= props.friend
+
   return (
-    <div style={personStyle}>
-      <h3>My Name: {props.name}</h3>
-      <p>My Profession: {props.job}</p>
+    <div style={productStyle}>
+      <h5>Name: {name} </h5>
+      <h4>Age: {age} </h4>
+      <h3>Job: {job}</h3>
+      <button>Buy Now</button>
     </div>
   )
 }
+ 
+
 
 export default App;
